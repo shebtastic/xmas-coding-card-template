df97e8178457917888cacb7fb2871a2e5e132458
Sheb <michael@sheb.rocks>
2022-12-08 11:42:39 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import {
+  createCardFlip,
+  createSnowfall,
+} from "https://raw.githack.com/shebtastic/xmas-coding-card/main/js/sketch.js";
+
+createCardFlip();
+createSnowfall({ count: 1, speed: 1, wind: 0, angularMomentum: 0.7 });
