e90fd7b47474324d497cc2687a3290c86ea61a4b
Sheb <michael@sheb.rocks>
2022-12-08 11:45:20 +0100

    initial blank

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
-import {
-  createCardFlip,
-  createSnowfall,
-} from "https://raw.githack.com/shebtastic/xmas-coding-card/main/js/sketch.js";
-
-createCardFlip();
-createSnowfall({ count: 1, speed: 1, wind: 0, angularMomentum: 0.7 });
+// import "https://raw.githack.com/shebtastic/xmas-coding-card/main/js/sketch.js";
