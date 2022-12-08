f28ae4942af1ba1c133e02e5060de5001c202fc8
Sheb <michael@sheb.rocks>
2022-12-08 12:07:59 +0100

    add snow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-import { createCardFlip } from "https://raw.githack.com/shebtastic/xmas-coding-card/main/js/sketch.js";
+import { createCardFlip, createSnowfall } from "./sketch.js";
 
 createCardFlip();
+createSnowfall({ count: 1, speed: 1, wind: 0, angularMomentum: 0.7 });
