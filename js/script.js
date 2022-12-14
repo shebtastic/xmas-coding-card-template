d9d4661fdfba976d1b49b6718bb3482ed85f5798
Sheb <michael@sheb.rocks>
2022-12-14 12:07:33 +0100

    add alert for missing classes, handle non existent css classes in flip, renamed lib, moved p5js back to cdn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { createCardFlip, createSnowfall } from "/.there-be-dragons/sketch.js";
+import { createCardFlip, createSnowfall } from "/lib/sketch.js";
 
 createCardFlip({ duration: 800 });
 createSnowfall({ count: 1, speed: 1, wind: 0, angularMomentum: 0.7 });
---
+++ lib/README.md
@@ -0,0 +1,5 @@
+# you found the backstage area!
+
+this is code we prepared for you.
+
+feel free to browse around, but keep in mind that we won't have to make any changes here during the event!
